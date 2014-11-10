b6266f58f8e6c44ea8ef25739b5e615606f8a748
alexbiju <alexmbiju@gmail.com>
2014-11-09 21:15:02 -0600

    Media server

--- MediaServer/src/main/java/com/MediaServer/MediaController.java
+++ MediaServer/src/main/java/com/MediaServer/MediaController.java
@@ -8,17 +8,20 @@ import static com.MediaServer.JsonUtil.*;
 public class MediaController {
     public MediaController(MediaService mediaService){
 
-        //search the media library
+        //searches the media library
         get("/search/:term",(req,res)->{
             String searchTerm = req.params(":term");
             return searchTerm;
         },json());
 
-        //returns top results
+        //returns top results? don't think i am doing this
         get("/top/",(req,res)->{
             return "hello";
         },json());
 
-
+        //send a file name/id and then respond with the url that is the file
+        get("/get/:fileName",(req,res)->{
+            return req.params(":fileName");
+        },json());
     }
 }
