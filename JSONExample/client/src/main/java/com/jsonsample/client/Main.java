f5176f708a684ea4686687b70a0e2e2f1ed10e51
alexbiju <alexmbiju@gmail.com>
2014-12-02 01:13:18 -0600

    new client for json

--- JSONExample/client/src/main/java/com/jsonsample/client/Main.java
+++ JSONExample/client/src/main/java/com/jsonsample/client/Main.java
@@ -1,10 +1,7 @@
 package com.jsonsample.client;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataOutputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -19,28 +16,28 @@ public class Main {
 	public static void main(String[] args) {
 		
 		try{
-			Socket socket = new Socket("localhost", 6000);
-			System.out.println("Connected to port 6000");
+			Socket socket = new Socket("localhost", 4545);
 			
 			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			
 			JSONObject toSend = new JSONObject();
-			toSend.put("Artist", "Taylor Swift");
-			toSend.put("Album", "1989");
+
+			toSend.put("Type", "Controller");
+			toSend.put("Request", "Top Ten");
+			
 			out.println(toSend.toString());
 			
-			
 			String received = in.readLine();
-			JSONObject jsonReceived = new JSONObject(new JSONTokener(received));
-			String soldString = jsonReceived.getString("sold");
-			System.out.println("Taylor Swift's album 1989 sold: " + soldString + " copies.");
+			System.out.print(received);
 			
 			toSend = new JSONObject();
 			toSend.put("Command", "shutdown");			
 			out.println(toSend.toString());
 			out.flush();
 			
+			out.close();
+			in.close();
 			socket.close();
 			
 		}catch(Exception ex){
