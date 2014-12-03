c9a65a6e805010660b14a538b73d9f6c5dd46338
alexbiju <alexmbiju@gmail.com>
2014-12-02 19:41:27 -0600

    render controller

--- Renderer/src/RenderController.java
+++ Renderer/src/RenderController.java
@@ -1,4 +1,67 @@
 
-public class RenderController {
 
+/**
+ * Created by alexbiju on 11/8/14.
+ */
+
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+
+public class RenderController  implements Runnable{
+	private ServerSocket serverSocket;
+	Socket socket = null;
+    public void run(){
+    	
+    	
+    	
+    	try {
+			serverSocket = new ServerSocket(4545);
+			System.out.println("Server Started");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	
+    	while(!Thread.currentThread().isInterrupted() && !serverSocket.isClosed()){
+    		try {
+    			
+				socket = serverSocket.accept();
+				System.out.println("Started socket");
+				
+				RenderService media = new RenderService("/Users/alexbiju/Desktop/Musi",this,socket);
+				new Thread(media).start();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+    	}
+    	if(serverSocket != null){
+        	try {
+        		socket.close();
+				serverSocket.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        }
+    }
+	public boolean isRunning(){
+		return serverSocket != null && !serverSocket.isClosed();
+	}
+	
+	public void close(){
+		if(serverSocket != null){
+			try {
+				socket.close();
+				serverSocket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+    
 }
+
+
