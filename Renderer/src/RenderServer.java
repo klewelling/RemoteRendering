b74f8cd0fe71ba1a88819dc7f0a791a1daabad96
alexbiju <alexmbiju@gmail.com>
2014-12-02 19:42:13 -0600

    sdcfas

--- Renderer/src/RenderServer.java
+++ Renderer/src/RenderServer.java
@@ -1,4 +1,35 @@
 
-public class RenderServer {
 
+
+/**
+ * Created by alexbiju on 11/8/14.
+ */
+
+import java.io.IOException;
+
+
+
+public class RenderServer {
+    
+
+    public static void main(String[] args) throws IOException {
+        RenderController rThread = new RenderController();
+        Thread mediaThread = new Thread(rThread);
+        mediaThread.start();
+        try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        while(rThread.isRunning()){
+        	try {
+				mediaThread.join(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        }
+
+    }
 }
